6349ba6b535bbe850a07d5c1644527b9bf2eb18a
Naveen A <2006naveen18@gmail.com>
2026-01-12 18:53:09 +0530

    Add files via upload

---
+++ MainClass.java
@@ -0,0 +1,30 @@
+// Outer class
+class OuterClass {
+
+    // Outer class method
+    void display() {
+        System.out.println("This is the display method of the Outer class.");
+    }
+
+    class InnerClass {
+
+        // Inner class method
+        void display() {
+            System.out.println("This is the display method of the Inner class.");
+        }
+    }
+}
+
+public class MainClass {
+
+    public static void main(String[] args) {
+
+        // Creating an instance of the outer class
+        OuterClass out = new OuterClass();
+        out.display();  // Calling the outer class's display method
+
+        // Creating an instance of the inner class
+        OuterClass.InnerClass in = out.new InnerClass();
+        in.display();   // Calling the inner class's display method
+    }
+}
