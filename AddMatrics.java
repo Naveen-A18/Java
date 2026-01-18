bff7cdba7e86b52cc4c6808b7b82e406d0a59ce1
Naveen A <2006naveen18@gmail.com>
2026-01-18 14:26:44 +0530

    Refactor AddMatrics.java for formatting and readability
    Fixed formatting issues and added missing line breaks in printMatrix method.

--- AddMatrics.java
+++ AddMatrics.java
@@ -79,10 +79,11 @@ public class AddMatrics {
             for (int value : row) {
                 System.out.print(value + "\t");
             }
-            
+            System.out.println();
         }
     }
 }
+
 
 
 
